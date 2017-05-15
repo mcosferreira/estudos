2a6217aaf96fe03222d224f326b5e6cd1e84427d
Marcos <Marcos@MarcosPC>
2017-05-15 07:23:01 -0300

    work

---
+++ jsfexample/jsfexample-web/src/main/java/br/msf/estudo/jsf/bean/HelloWorldBean.java
@@ -0,0 +1,35 @@
+package br.msf.estudo.jsf.bean;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="helloWorldBean")
+@SessionScoped
+public class HelloWorldBean implements Serializable {
+
+	private static final long serialVersionUID = -9215638431730052821L;
+	private String nome;
+
+	public HelloWorldBean() {
+		super();
+	}
+	
+	public String hello() {
+		return nome;
+	}
+
+	public String currentTime() {
+		return (LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MMM yyyy:HH:mm:SS"))); 
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
