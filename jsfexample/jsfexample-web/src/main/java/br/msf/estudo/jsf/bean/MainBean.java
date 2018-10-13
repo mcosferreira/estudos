5f02d0810d60213d6e24ca7ebd0595105d82eae0
Marcos S. Ferreira <msferreira.prof@gmail.com>
2018-10-12 23:32:45 -0300

    Ajustes - Aulas Secao 02

--- jsfexample/jsfexample-web/src/main/java/br/msf/estudo/jsf/bean/MainBean.java
+++ jsfexample/jsfexample-web/src/main/java/br/msf/estudo/jsf/bean/MainBean.java
@@ -3,10 +3,10 @@ package br.msf.estudo.jsf.bean;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
 
 @ManagedBean
-@ViewScoped
+@RequestScoped
 public class MainBean implements Serializable {
 
 	private static final long serialVersionUID = 1080155375918901553L;
