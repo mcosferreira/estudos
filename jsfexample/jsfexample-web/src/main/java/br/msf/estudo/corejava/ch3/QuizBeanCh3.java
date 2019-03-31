94bfc9c2ea637d73de2a5e5816e765ded190d552
Marcos S. Ferreira <msferreira.prof@gmail.com>
2019-03-30 21:02:35 -0300

    ch3

--- jsfexample/jsfexample-web/src/main/java/br/msf/estudo/corejava/ch3/QuizBeanCh3.java
+++ jsfexample/jsfexample-web/src/main/java/br/msf/estudo/corejava/ch3/QuizBeanCh3.java
@@ -6,80 +6,87 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-import javax.inject.Named; 
-   // or import javax.faces.bean.ManagedBean;
-import javax.enterprise.context.SessionScoped; 
-   // or import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+// or import javax.faces.bean.ManagedBean;
+import javax.enterprise.context.SessionScoped;
+// or import javax.faces.bean.SessionScoped;
 
 @Named // or @ManagedBean
 @SessionScoped
 public class QuizBeanCh3 implements Serializable {
-   private int currentProblem;
-   private int tries;
-   private int score;
-   private String response = "";
-   private String correctAnswer;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6649981791708095946L;
+	private int currentProblem;
+	private int tries;
+	private int score;
+	private String response = "";
+	private String correctAnswer;
 
-   // Here, we hardwire the problems. In a real application,
-   // they would come from a database
-   private ArrayList<Problem> problems = new ArrayList<Problem>(Arrays.asList(
-      new Problem(
-         "What trademarked slogan describes Java development? Write once, ...",
-         "run anywhere"),
-      new Problem(
-         "What are the first 4 bytes of every class file (in hexadecimal)?",
-         "CAFEBABE"),
-      new Problem(
-         "What does this statement print? System.out.println(1+\"2\");",
-         "12"),
-      new Problem(
-         "Which Java keyword is used to define a subclass?",
-         "extends"),
-      new Problem(
-         "What was the original name of the Java programming language?",
-         "Oak"),
-      new Problem(
-         "Which java.util class describes a point in time?",
-         "Date")));
+	// Here, we hardwire the problems. In a real application,
+	// they would come from a database
+	private ArrayList<Problem> problems = new ArrayList<Problem>(Arrays.asList(
+			new Problem("What trademarked slogan describes Java development? Write once, ...", "run anywhere"),
+			new Problem("What are the first 4 bytes of every class file (in hexadecimal)?", "CAFEBABE"),
+			new Problem("What does this statement print? System.out.println(1+\"2\");", "12"),
+			new Problem("Which Java keyword is used to define a subclass?", "extends"),
+			new Problem("What was the original name of the Java programming language?", "Oak"),
+			new Problem("Which java.util class describes a point in time?", "Date")));
 
-   public String getQuestion() { return problems.get(currentProblem).getQuestion(); }
+	public String getQuestion() {
+		return problems.get(currentProblem).getQuestion();
+	}
 
-   public String getAnswer() { return correctAnswer; }
+	public String getAnswer() {
+		return correctAnswer;
+	}
 
-   public int getScore() { return score; }
+	public int getScore() {
+		return score;
+	}
 
-   public String getResponse() { return response; }
-   public void setResponse(String newValue) { response = newValue; }
+	public String getResponse() {
+		return response;
+	}
 
-   public String answerAction() {
-      tries++;
-      if (problems.get(currentProblem).isCorrect(response)) {
-         score++;
-         nextProblem();
-         if (currentProblem == problems.size()) return "done";
-         else return "success";
-      }
-      else if (tries == 1) return "again";
-      else {
-         nextProblem();
-         if (currentProblem == problems.size()) return "done";
-         else return "failure";
-      }
-   }
+	public void setResponse(String newValue) {
+		response = newValue;
+	}
 
-   public String startOverAction() {
-      Collections.shuffle(problems);
-      currentProblem = 0;
-      score = 0;
-      tries = 0;
-      response = "";
-      return "startOver";
-   }
+	public String answerAction() {
+		tries++;
+		if (problems.get(currentProblem).isCorrect(response)) {
+			score++;
+			nextProblem();
+			if (currentProblem == problems.size())
+				return "done";
+			else
+				return "success";
+		} else if (tries == 1)
+			return "again";
+		else {
+			nextProblem();
+			if (currentProblem == problems.size())
+				return "done";
+			else
+				return "failure";
+		}
+	}
 
-   private void nextProblem() {
-      correctAnswer = problems.get(currentProblem).getAnswer();
-      currentProblem++;
-      tries = 0;
-      response = "";
-   }
+	public String startOverAction() {
+		Collections.shuffle(problems);
+		currentProblem = 0;
+		score = 0;
+		tries = 0;
+		response = "";
+		return "startOver";
+	}
+
+	private void nextProblem() {
+		correctAnswer = problems.get(currentProblem).getAnswer();
+		currentProblem++;
+		tries = 0;
+		response = "";
+	}
 }
